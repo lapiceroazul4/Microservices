44fb395c88a6600b5bc6de5ddd3d193fb8383cd8
lapiceroazul4 <lapiceroazul@proton.me>
2024-03-21 12:30:18 -0500

    adding micro_reservas

---
+++ microservices/micro_reservas/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const reservasController = require('./controllers/reservasController.js'); 
+const morgan = require('morgan');
+const cors= require('cors')
+
+const app = express();
+app.use(cors())
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(reservasController);
+app.listen(3003, () => {
+console.log('Microservicio de Reservas ejecutandose en el puerto 3003');
+});
